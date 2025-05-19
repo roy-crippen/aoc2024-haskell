b63739501d2ea7d8692c6bd629200b6baac7d598
roycrippen <roycrippen@gmail.com>
2025-05-18 23:14:12 -0500

    better names

--- lib/Day13.hs
+++ lib/Day13.hs
@@ -22,11 +22,11 @@ parse s = map parseAbv $ chunkList 3 $ filter (\t -> T.length t > 0) (T.lines s)
         (s1, s2, s3) = case xs of
           [t1, t2, t3] -> (t1, t2, t3)
           _ -> error "Invalid input"
-        (x1, y1) = parseValues s1 "+"
-        (x2, y2) = parseValues s2 "+"
+        (a1, a2) = parseValues s1 "+"
+        (b1, b2) = parseValues s2 "+"
         (v1, v2) = parseValues s3 "="
-        abv1 = (x1, x2, v1)
-        abv2 = (y1, y2, v2)
+        abv1 = (a1, b1, v1)
+        abv2 = (a2, b2, v2)
 
 parseValues :: T.Text -> String -> (Int, Int)
 parseValues t s = case map (T.splitOn (T.pack s)) (T.splitOn (T.pack ",") t) of
