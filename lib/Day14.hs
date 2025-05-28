ea4c26ade80839df449a352cc707f3a89c323795
roycrippen <roycrippen@gmail.com>
2025-05-28 17:16:57 -0500

    speedup day 14 part 2

--- lib/Day14.hs
+++ lib/Day14.hs
@@ -3,13 +3,16 @@
 
 module Day14 where
 
--- import Control.Parallel.Strategies (parMap, rseq)
+import Control.Monad.ST (ST, runST)
 import Data.FileEmbed (embedFile)
-import Data.List (group, sort, sortBy)
+import Data.List (foldl', sortBy)
 import Data.Ord (comparing)
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
-import Debug.Trace (trace)
+import Data.Vector.Unboxed (Vector)
+import Data.Vector.Unboxed qualified as V
+import Data.Vector.Unboxed.Mutable (MVector)
+import Data.Vector.Unboxed.Mutable qualified as VM
 import Text.Heredoc (here)
 import Util (Solution (..), parseNumUnsafe)
 
@@ -21,8 +24,10 @@ type Length = Int
 
 type Count = Idx
 
-parse :: T.Text -> ([(Idx, Delta)], [(Idx, Delta)])
-parse s = unzip . sortYX $ map parseValues (T.lines s)
+parse :: T.Text -> (Vector (Idx, Delta), Vector (Idx, Delta))
+parse s = (V.fromList xs, V.fromList ys)
+  where
+    (xs, ys) = unzip . sortYX $ map parseValues (T.lines s)
 
 parseValues :: T.Text -> ((Idx, Delta), (Idx, Delta))
 parseValues s = ((x, vx), (y, vy))
@@ -41,15 +46,13 @@ parseValue s = (v1, v2)
 sortYX :: [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))]
 sortYX = sortBy (\(x1, y1) (x2, y2) -> comparing fst y1 y2 <> comparing fst x1 x2)
 
-process :: [(Idx, Delta)] -> Length -> Count -> [Idx]
-process pairs len count = map (\(x, vx) -> (x + vx * count) `mod` len) pairs
+process :: Vector (Idx, Delta) -> Length -> Count -> Vector Idx
+process pairs len count = V.map (\(x, vx) -> (x + vx * count) `mod` len) pairs
 
 scorePart1 :: [(Int, Int)] -> Int -> Int -> Int
 scorePart1 xs midX midY = q1 * q2 * q3 * q4
   where
-    -- msg = "count (q1,q2,q3,q4) = (" ++ show q1 ++ "," ++ show q2 ++ "," ++ show q3 ++ "," ++ show q4 ++ ")"
-    (q1, q2, q3, q4) = foldl getScore (0, 0, 0, 0) xs
-
+    (q1, q2, q3, q4) = foldl' getScore (0, 0, 0, 0) xs
     getScore :: (Int, Int, Int, Int) -> (Int, Int) -> (Int, Int, Int, Int)
     getScore (q1', q2', q3', q4') (x, y) = case (lowX, lowY) of
       (True, True) -> (q1' + 1, q2', q3', q4')
@@ -82,23 +85,39 @@ part1 s = scorePart1 filteredPts midX midY
     midX = cols `div` 2
     midY = rows `div` 2
     (xs, ys) = parse s
-    xyPairs = zip (process xs cols 100) (process ys rows 100)
+    xyPairs = V.toList $ V.zip (process xs cols 100) (process ys rows 100)
     filteredPts = filter (\(x, y) -> x /= midX && y /= midY) xyPairs
 
 part2 :: T.Text -> Int
-part2 s = go 6000 -- needs fixing
--- faster sort container -> Data.Vector.Unboxed Bool
+part2 s = runST $ do
+  xFreq <- VM.replicate cols 0
+  yFreq <- VM.replicate rows 0
+  go 1 xFreq yFreq
   where
-    go :: Count -> Count
-    go count =
-      if length xGroups > 1 && length yGroups > 10
-        then count -- trace (show count ++ ", " ++ show (sort xIndexes))
-        else go (count + 1)
-      where
-        xIndexes = process xs cols count
-        xGroups = filter (\vs -> length vs > 30) $ group (sort xIndexes)
-        yIndexes = process ys rows count
-        yGroups = filter (\vs -> length vs > 10) $ group (sort yIndexes)
+    go :: Count -> MVector s Int -> MVector s Int -> ST s Count
+    go count xFreq yFreq = do
+      VM.set xFreq 0
+      VM.set yFreq 0
+      let xIndexes = process xs cols count
+          yIndexes = process ys rows count
+      V.forM_ xIndexes $ \i -> VM.unsafeModify xFreq (+ 1) i
+      V.forM_ yIndexes $ \i -> VM.unsafeModify yFreq (+ 1) i
+      xGroupCount <- countGroups xFreq 30
+      yGroupCount <- countGroups yFreq 10
+      if xGroupCount >= 2 && yGroupCount >= 10
+        then return count
+        else go (count + 1) xFreq yFreq
+
+    countGroups :: MVector s Int -> Int -> ST s Int
+    countGroups v minCount = do
+      let n = VM.length v
+      let scan i acc =
+            if i >= n
+              then return acc
+              else do
+                val <- VM.unsafeRead v i
+                scan (i + 1) (if val >= minCount then acc + 1 else acc)
+      scan 0 0
 
     rows = 103
     cols = 101
